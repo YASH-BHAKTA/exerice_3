425a22e8ffb91a10275aa02f8843fa8f622d2ec9
yash-bhakta <ybhakta1000@gmail.com>
2023-12-05 10:06:44 -0400

    another css changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
 	const fetchDataButton = document.getElementById('fetch-data');
 	const dogDataContainer = document.getElementById('dog-data');
